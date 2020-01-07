6402ee569e7d4b7cabae4914737b56eb891515a8
Bryan Jenks <thenewpheonix@gmail.com>
2020-01-06 21:53:25 -0800

    more edits setting up data import structure

---
+++ r/edu.r
@@ -0,0 +1,2 @@
+test <- read.csv(file = "~/GithubRepos/CV/data/edu.csv", header = T, sep = ",") %>%
+    as_tibble()
---
+++ r/work.r
@@ -0,0 +1,2 @@
+test <- read.csv(file = "~/GithubRepos/CV/data/work.csv", header = T, sep = ",") %>%
+    as_tibble()
